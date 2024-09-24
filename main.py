010aa0243de7bc58a646cf996566e348e79eb574
usda <daniel.uszta@gmail.com>
2024-09-24 13:07:32 +0200

    added asteroid spawning and collision logic

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -9,10 +11,14 @@ def main():
 
     drawable = pygame.sprite.Group()
     updatable = pygame.sprite.Group()
+    asteroid = pygame.sprite.Group()
     Player.containers = (drawable, updatable)
+    Asteroid.containers = (drawable,updatable,asteroid)
+    AsteroidField.containers = (updatable)
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT /2)
+    asteroid_field = AsteroidField()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -23,6 +29,10 @@ def main():
             item.draw(screen)
         for item in updatable:
             item.update(dt)
+        for item in asteroid:
+            if item.collision(player) == True:
+                print("Game over!")
+                exit()
         pygame.display.flip()
 
 
