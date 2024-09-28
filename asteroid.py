8b1f0e4592a3c65f2e86a7124518c89234b8b001
usda <daniel.uszta@gmail.com>
2024-09-28 15:00:39 +0200

    Added collisions, shooting, asteroids exploding, asteroids splitting into smaller asteroids

--- asteroid.py
+++ asteroid.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 from constants import *
 from circleshape import CircleShape
 
@@ -10,4 +11,17 @@ class Asteroid(CircleShape):
         pygame.draw.circle(screen,"white",self.position,self.radius,2)
     def update(self,dt):
         self.position += self.velocity * dt
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        else:
+            new_radius = self.radius - ASTEROID_MIN_RADIUS
+            new_vector_1 = self.velocity.rotate(random.uniform(20,50))
+            new_vector_2 = self.velocity.rotate(-random.uniform(20,50))
+            asteroid_1 = Asteroid(self.position,new_radius,new_radius)
+            asteroid_1.velocity = new_vector_1 * 1.2
+            asteroid_2 = Asteroid(self.position,new_radius,new_radius)
+            asteroid_2.velocity = new_vector_2 * 1.2
+            
 
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
@@ -12,9 +13,13 @@ def main():
     drawable = pygame.sprite.Group()
     updatable = pygame.sprite.Group()
     asteroid = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
     Player.containers = (drawable, updatable)
     Asteroid.containers = (drawable,updatable,asteroid)
     AsteroidField.containers = (updatable)
+    Shot.containers = (shots, drawable, updatable)
+
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT /2)
@@ -29,12 +34,17 @@ def main():
             item.draw(screen)
         for item in updatable:
             item.update(dt)
+        
         for item in asteroid:
             if item.collision(player) == True:
                 print("Game over!")
                 exit()
-        pygame.display.flip()
+            for shot in shots:
+                if item.collision(shot) == True:
+                    item.split()
+                    shot.kill()
 
+        pygame.display.flip()
 
         dt = clock.tick(60) / 1000
 
